deb49768c00178acfadc45dba6dfddad0266f1e4
wctoat <wctoat@163.com>
2022-09-12 16:16:00 +0800

    file

--- sql.py
+++ sql.py
@@ -4,3 +4,7 @@ conn = pymysql.connect(host='localhost',
                        database='word',
                        user='root',
                        password='root1234')
+
+class DataBase:
+    def __init__(self):
+        self.cur = conn.cursor()
